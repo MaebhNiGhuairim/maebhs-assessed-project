a1c92c153821cf5aa24eae388e0cf13ba185287a
Maebh Ni <mnighuairim@gmail.com>
2025-01-18 18:04:41 +0000

    added env to settings

--- the_yoga_loft/settings.py
+++ the_yoga_loft/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bwbr@_^-%aaxv^yfd%m7up%p3yfdbf@bg!nsg$&i&b$x)3%xr*'
+import os
+if os.path.isfile("env.py"):
+    import env
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
