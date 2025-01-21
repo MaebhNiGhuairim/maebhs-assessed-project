76dde5ee4eee106136f784fd0cb7074155a5be21
Maebh Ni <mnighuairim@gmail.com>
2025-01-21 15:23:46 +0000

    added static collect

--- the_yoga_loft/settings.py
+++ the_yoga_loft/settings.py
@@ -134,6 +134,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "home/static",
 ]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "home/static"),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
