9e42084938cdbca65a6835aea6e7ffac6471b0c2
Maebh Ni <mnighuairim@gmail.com>
2025-01-22 22:56:38 +0000

    added admin access to contact messages

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'submitted_at')
+    search_fields = ('name', 'email')
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
